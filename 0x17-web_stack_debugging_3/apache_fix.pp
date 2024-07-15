37befa2d51b90f1895de6a9fd10218091adc9bab
Patricia8027 <akenahpatrciaapanyen@gmail.com>
2024-07-15 14:59:07 -0700

    total fixed

--- 0x17-web_stack_debugging_3/apache_fix.pp
+++ 0x17-web_stack_debugging_3/apache_fix.pp
@@ -1,31 +1,53 @@
-# Update package lists
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update',
-  before  => Package['php5-curl'],
-}
+# This Puppet manifest ensures the proper configuration of the Apache web server
 
-# Install necessary packages
-package { ['apache2', 'php5']:
+# Install the Apache package
+package { 'apache2':
   ensure => installed,
 }
 
-# Install php5-curl package
-package { 'php5-curl':
-  ensure   => installed,
-  provider => apt,
-  require  => Exec['apt-get update'],
+# Configure the Apache configuration file
+file { '/etc/apache2/apache2.conf':
+  ensure  => file,
+  content => template('apache/apache2.conf.erb'),
+  notify  => Service['apache2'],
 }
 
-# Restart Apache service
+# Template for the Apache configuration file
+template 'apache/apache2.conf.erb' {
+  content => @(END)
+    # This is the main Apache server configuration file.
+    # It contains the configuration directives that
+    # give the server its instructions.
+
+    # Global Configuration
+    ServerRoot "/etc/apache2"
+    PidFile ${APACHE_PID_FILE}
+    Timeout 300
+    KeepAlive On
+    MaxKeepAliveRequests 100
+    KeepAliveTimeout 5
+
+    # Multi-Processing Module (MPM)
+    IncludeOptional mods-enabled/*.load
+    IncludeOptional mods-enabled/*.conf
+
+    # Modules
+    LoadModule mpm_prefork_module modules/mod_mpm_prefork.so
+    LoadModule authz_core_module modules/mod_authz_core.so
+    LoadModule auth_basic_module modules/mod_auth_basic.so
+    LoadModule unixd_module modules/mod_unixd.so
+    LoadModule dir_module modules/mod_dir.so
+    LoadModule mime_module modules/mod_mime.so
+
+    # Virtual Hosts
+    IncludeOptional sites-enabled/*.conf
+  END
+}
+
+# Ensure the Apache service is running and enabled
 service { 'apache2':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  hasstatus  => true,
-  subscribe  => [
-    Package['php5'],
-    Package['php5-curl'],
-  ],
+  ensure => running,
+  enable => true,
 }
 
 
